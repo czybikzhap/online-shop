37d7f80abef4cbabda42f7b0c48f14ce026b4cab
czybikzhap <czybikzhap@gmail.com>
2023-07-10 14:32:46 +0700

    исправил валидацию SignUp

--- app/public/index.php
+++ app/public/index.php
@@ -1,170 +1,148 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    $conn = new PDO('pgsql:host=db; dbname=dbname', 'dbuser', 'dbpwd');
-    $errors = isValid($_POST, $conn);
+$requestUri = $_SERVER["REQUEST_URI"];
 
-    if (empty($errors)) {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $repeat_pwd = $_POST['repeat_pwd'];
+if($requestUri === '/signup') {
+    function isValidSignUp(array $data, PDO $conn): array
+    {
+        $errors = [];
 
-        $password = password_hash($password, PASSWORD_DEFAULT);
-
-        $stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
-        $stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
-
-        $stmt = $conn->prepare("SELECT * FROM users WHERE name = :name");
-        $stmt->execute(['name' => $name]);
-        $dbinfo = $stmt->fetch();
-
-        print_r('id - ' . $dbinfo['id']);
-        echo '<br>';
-        print_r('name - ' . $dbinfo['name']);
-        echo '<br>';
-        print_r('email - ' . $dbinfo['email']);
-        echo '<br>';
-        print_r('password - ' . $dbinfo['password']);
+        if (isset($name)) {
+            $errors['name'] = 'name is required';
+        }
+        $name = $data['name'];
+        if (empty($name)){
+            $errors['name'] = 'email не может быть пустым';
+        }
+        if (strlen($name) < 2) {
+            $errors['name'] = 'name должен содержать больше 2-х символов';
         }
 
+        if (isset($email)){
+            $errors['email'] = 'email is required';
+        }
+        $email = $data['email'];
+        if (empty($email)){
+            $errors['email'] = 'email не может быть пустым';
+        }
+        if (strlen($email) < 2){
+            $errors['email'] = 'email должен содержать больше 2-х символов';
+        }
+
+        if (isset($password)){
+            $errors['password'] = 'password is required';
+        }
+        $password = $data['password'];
+        if (empty($password)){
+            $errors['password'] = 'password не может быть пустым';
+        }
+        if (strlen($password) < 2){
+            $errors['password'] = 'password должен содержать больше 2-х символов';
+        }
+        if (isset($repeat_pwd)){
+            $errors['repeat_pwd'] = 'password is required';
+        }
+        $repeat_pwd = $data['repeat_pwd'];
+        if (empty($repeat_pwd)){
+            $errors['repeat_pwd'] = 'password не может быть пустым';
+        }
+        if ($password !== $repeat_pwd){
+            $errors['repeat_pwd'] = 'пароли не совадают';
+        }
+
+        $stmt = $conn->prepare("SELECT * FROM users WHERE email = :email ");
+        $stmt->execute(['email' => $email]);
+        $userData = $stmt->fetch();
+        if (!empty($userData)) {
+            $errors['email'] = 'пользователь с таким адресом электронной почты уже зарегистрирован';
+        }
+        return $errors;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
+        $conn = new PDO('pgsql:host=db; dbname=dbname', 'dbuser', 'dbpwd');
+        $errors = isValidSignUp($_POST, $conn);
+
+        if (empty($errors)) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $repeat_pwd = $_POST['repeat_pwd'];
+
+            $password = password_hash($password, PASSWORD_DEFAULT);
+
+            $stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+            $stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
+
+            $stmt = $conn->prepare("SELECT * FROM users WHERE name = :name");
+            $stmt->execute(['name' => $name]);
+            $dbinfo = $stmt->fetch();
+
+            print_r('id - ' . $dbinfo['id']);
+            echo '<br>';
+            print_r('name - ' . $dbinfo['name']);
+            echo '<br>';
+            print_r('email - ' . $dbinfo['email']);
+            echo '<br>';
+            print_r('password - ' . $dbinfo['password']);
+        }
+
+    }
+
+    require_once './views/signup.html';
+
+} elseif ($requestUri === '/login') {
+  function isValidLogin(array $data, PDO $conn): array {
+
+        $errors = [];
+        if (!isset($data['email'])) {
+            $errors['email'] = 'email is required';
+        }
+        $email = $data['email'];
+        if (empty($email)){
+            $errors['email'] = 'email не может быть пустым';
+        }
+        if (!isset($data['password'])){
+            $errors['password'] = 'password is required';
+        }
+        $password = $data['password'];
+        if (empty($password)){
+            $errors['password'] = 'password не может быть пустым';
+        }
+
+        $stmt = $conn->prepare("SELECT * FROM users WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        $dbinfo = $stmt->fetch();
+        $hash = $dbinfo['password'];
+
+        if (empty($dbinfo['email']) or !password_verify($data['password'], $hash)) {
+            $errors['email'] = 'неверное имя пользователя или пароль';
+        }
+        return $errors;
+    }
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
+        $conn = new PDO('pgsql:host=db; dbname=dbname', 'dbuser', 'dbpwd');
+
+        $errors = isValidLogin($_POST, $conn);
+        $user = 1;
+        session_start();
+        $_SESSION['id'] = $user;
+
+
+    }
+
+
+} elseif ($requestUri === '/main') {
+    session_start();
+    echo $_SESSION['id'];
+
+} else {
+echo 'not found';
 }
 
-function isValid(array $data, PDO $conn): array
-{
-    $errors = [];
-    $name = $data['name'];
-    $email = $data['email'];
-    $password = $data['password'];
-    $repeat_pwd = $data['repeat_pwd'];
 
-    if (!isset($name)) {
-        $errors[$name] = 'name is required';
-    } elseif (strlen($name) < 2) {
-        $errors['name'] = 'name должен содержать больше 2-х символов';
-    }
-    elseif (!isset($email)){
-        $errors[$email] = 'email is required';
-    } elseif (empty($email)){
-        $errors[$email] = 'email не может быть пустым';
-    } elseif (strlen($email) < 2){
-        $errors['email'] = 'email должен содержать больше 2-х символов';
-    }
-    elseif (!isset($password)){
-        $errors['password'] = 'password is required';
-    } elseif (empty($password)){
-        $errors['password'] = 'password не может быть пустым';
-    } elseif (strlen($password) < 2){
-        $errors['password'] = 'password должен содержать больше 2-х символов';
-    }
-    elseif ($password !== $repeat_pwd){
-        $errors['repeat_pwd'] = 'пароли не совадают';
-    }
-
-    $stmt = $conn->prepare("SELECT * FROM users WHERE email = :email ");
-    $stmt->execute(['email' => $email]);
-    $userData = $stmt->fetch();
-    if (!empty($userData)) {
-        $errors['email'] = 'пользователь с таким адресом электронной почты уже зарегистрирован';
-    }
-    return $errors;
-}
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Job Application Registration Form</title>
-    <style>
-        body {
-            font-family: Calibri, sans-serif;
-            background-color: #72b7f4;
-        }
-        h2 {
-            color: #232729;
-            text-align: center; /* Center align the title */
-        }
-        form {
-            background-color: #bfddf7;
-            max-width: 500px;
-            margin: 0 auto;
-            padding: 30px;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        label {
-            display: block;
-            margin-bottom: 10px;
-            color: #333333;
-        }
-        input[type="text"],
-        input[type="email"],
-        input[type="tel"],
-        input[type="file"] {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #cccccc;
-            border-radius: 4px;
-            box-sizing: border-box;
-            margin-bottom: 15px;
-            background-color: #ffffff; /* Set background color to white */
-        }
-        select {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #cccccc;
-            border-radius: 4px;
-            box-sizing: border-box;
-            margin-bottom: 15px;
-        }
-        input[type="submit"] {
-            background-color: #1a73e8;
-            color: #ffffff;
-            border: none;
-            padding: 12px 24px;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-        input[type="submit"]:hover {
-            background-color: #0059b3;
-        }
-    </style>
-</head>
-<body>
-<h2>Job Application Registration</h2>
-<form action="" method="POST">
-    <label for="name">Full Name:</label>
-    <label style="color:red"><?php
-        if (isset ($errors['name'])) {
-            echo $errors['name'];
-        }
-        ?></label>
-    <input type="text" id="name" name="name" required>
-    <label for="email">Email Address:</label>
-    <label style="color:red"><?php
-        if (isset ($errors['email'])) {
-            echo $errors['email'];
-        }
-        ?></label>
-    <input type="email" id="email" name="email" required>
-    <label for="password">Password:</label>
-    <label style="color:red"><?php
-        if (isset ($errors['password'])) {
-            echo $errors['password'];
-        }
-        ?></label>
-    <input type="password" id="userPassword" name="password" required>
-    <label for="userPassword">Repeat Password:</label>
-    <label style="color:red"><?php
-        if (isset ($errors['repeat_pwd'])) {
-            echo $errors['repeat_pwd'];
-        }
-        ?></label>
-    <input type="password" id="userPassword" name="repeat_pwd" required>
-    <label><?php echo '<br>'; ?></label>
-    <input type="submit" value="Submit Application">
-</form>
-</body>
-</html>
+
 
 
 
